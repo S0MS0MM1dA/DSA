fed23e0315e0e51fe4f7b6e2b9a6c882bdea09c5
Durjoy Podder <126905695+S0MS0MM1dA@users.noreply.github.com>
2025-11-05 01:33:48 +0600

    Add files via upload

--- Recursion/6.Subsequence.cpp
+++ Recursion/6.Subsequence.cpp
@@ -4,15 +4,24 @@ using namespace std;
 // first take, then next -> take next...
 // the not take next -> not take next...
 
+// TC = O(N*2^n) (Recursion call 2^N )
+// SC = O(n) - Depth
 void print_subsequences(int ind, int arr[], int n, vector<int> v){
 
     if(ind >= n){
+
+        // TC = O(n)
         for(int i=0; i<v.size(); i++){
             cout<<v[i]<<' ';
+        }
+        //NULL
+        if(v.size() == 0){
+            cout<<"{}"<<'\n';
         }
         cout<<'\n';
         return;
     }
+
     //take the particular index into the subsequence
     v.push_back(arr[ind]);
     print_subsequences(ind+1, arr, n, v);
